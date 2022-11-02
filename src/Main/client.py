93f3c0a898fdf84fb0851812f4760ecf8c35eb78
Guilherme Varela <guilherme.varelaaa@gmail.com>
2022-11-02 23:10:53 +0000

    Cliente done (?)

--- src/Main/client.py
+++ src/Main/client.py
@@ -1,12 +1,36 @@
-# Author: Miguel Braga
+# Author: Guilherme Varela
 # Created at: 29/10/22
-# Last update: 29/10/12
+# Last update: 02/11/12
 # Description: 
 
 import socket
+import sys
+from src.QueryMessage.message import build_message
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def main():
+    args = sys.argv
+    args.pop(0)
 
-msg = "Teste"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-s.sendto(msg.encode('utf-8'), ('127.0.0.1', 3333))
+    args_split = args[0].split(":")
+    ip_address = args_split[0]
+
+    if len(args_split) > 1:
+        port = args_split[1]
+    else:
+        port = 5353 # porta default
+
+    name = args[1]
+    type_of_value = args[2]
+    flag = None
+    if len(args) == 4:
+        flag = args[3]
+
+    message = build_message(name, type_of_value, flag)
+
+    s.sendto(message.encode('utf-8'), (ip_address, port))
+
+
+if __name__ == "__main__" :
+    main()
--- src/QueryMessage/message.py
+++ src/QueryMessage/message.py
@@ -3,8 +3,20 @@
 # Last update: 30/10/12
 # Description: PDU of a query: Header, Data
 
-from header import Header
-from data import Data
+from src.QueryMessage.header import Header
+from src.QueryMessage.data import Query_Data
+
+def build_message(name, type_of_value, flag):
+    if flag == None:
+        flag = "Q+R"
+    else:
+        flag = "Q"
+
+    message = "1," + flag + ",0,0,0,0;" + name + "," + type_of_value + ";"
+    print(message)
+
+    return message
+
 
 class Message:
     def __init__(self, header, data):
