53c8fecfdadaa1081ab464d5b3496cb3afdf500a
gabrielasfc <gsfcunha@gmail.com>
2022-11-06 01:04:07 +0000

    tcp connection on zone transfer working

--- src/server.py
+++ src/server.py
@@ -6,6 +6,7 @@
 
 import socket
 import sys
+import threading
 
 from parser import *
 from query_message.message import *
@@ -28,9 +29,6 @@ class Server:
         else:
             self.server_type = "SR"
 
-        if self.server_type == "SS":
-            self.zone_transfer_initial()
-
         self.root_servers = parser_st(rfp)
         self.db = parser_df(d_fp)
         self.addresses_from = dict() #estrutura para saber onde mandar a query com message id X
@@ -47,79 +45,102 @@ class Server:
                f"Domínios por defeito: {self.default_domains}\nTipo do servidor: {self.server_type}\nRoot Servers:" \
                f"{self.root_servers}\nFicheiro de Log: {self.log_file_path}"
 
-    def zone_transfer_initial(self):
+    def zone_transfer_sp(self):
+        socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket TCP
+        socket_tcp.bind(("127.0.0.1", 8000))
+        socket_tcp.listen()
+
+        while True:
+            connection, address = socket_tcp.accept()
+
+            msg = connection.recv(1024)
+
+            if not msg:
+                break
+
+            print(msg.decode('utf-8'))
+
+        connection.close()
+
+    def zone_transfer_ss(self):
         sp = self.primary_server
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         substrings = sp.split(':')
         ip_address = substrings[0]
-        port = substrings[1]
+        port = int(substrings[1])
 
-        s.bind((ip_address, port))
-        s.listen()
+        socket_tcp.connect((ip_address, port))
 
         print(f"Estou à escuta no {ip_address}:{port}")
 
-        while True:
-            connection, address = s.accept()  # 3-way handshake
-            msg = "zone transfer"
-            connection.sendall(msg.encode('utf-8'))
-            response = connection.recv(1024)
+        msg = "zone transfer"
+        socket_tcp.sendall(msg.encode('utf-8'))
+        #response = socket_tcp.recv(1024)
 
     def add_address(self, message_id, address):
+        """
+        Adiciona um endereço para onde a query com message id tem de retornar
+        :param message_id: Message id da query
+        :param address: Endereço a adicionar
+        :return: void
+        """
         self.addresses_from[message_id] = address
 
     def get_address(self, message_id):
         return self.addresses_from[message_id]
+
     def response_query(self, query): #objeto do tipo message
-            response_values = self.db.get_values_by_type_and_parameter(type_of_value, name)
+        (message_id, flags, name, type_of_value) = parse_message(query)
+        response_values = self.db.get_values_by_type_and_parameter(type_of_value, name)
 
-            if len(response_values) != 0:     # HIT
-                authorities_values = self.db.get_values_by_type_and_parameter("NS", name)
-                extra_values = list()
+        if len(response_values) != 0:     # HIT
+            authorities_values = self.db.get_values_by_type_and_parameter("NS", name)
+            extra_values = list()
 
-                for data_entry in response_values:
-                    if data_entry.value in self.db.get_parameter_keys("A"):
-                        extra_values += self.db.get_values_by_type_and_parameter("A", data_entry.value)
-                for data_entry in authorities_values:
-                    if data_entry.value in self.db.get_parameter_keys("A"):
-                        extra_values += self.db.get_values_by_type_and_parameter("A", data_entry.value)
+            for data_entry in response_values:
+                if data_entry.value in self.db.get_parameter_keys("A"):
+                    extra_values += self.db.get_values_by_type_and_parameter("A", data_entry.value)
+            for data_entry in authorities_values:
+                if data_entry.value in self.db.get_parameter_keys("A"):
+                    extra_values += self.db.get_values_by_type_and_parameter("A", data_entry.value)
 
-                print(extra_values)
-                response = build_query_response(query, response_values, authorities_values, extra_values)
+            response = build_query_response(query, response_values, authorities_values, extra_values)
 
-                return (True, response)
+            return response
 
-            else:   # SS
-                if self.server_type == "SS": # ver se o parametro da query é subdominio do dominio do servidor
-                                             # se for, contactar SP
-                                             # caso contrario, ir aos ST
-                    return (False, self.primary_server) # Caso não encontra, devolve a query original e o ip para onde tem de mandar a query
+        else:   # MISS
+            return None
 
 
 def main():
     args = sys.argv
     config_filepath = args[1]
-    port = int(args[2])
+    port = args[2]
     timeout = args[3]
     mode = args[4]
+    ip_address = '127.0.0.1'
 
-    #if not validate_port(port):
-    #    return #adicionar log
+    if not validate_port(port):
+        return #adicionar log
 
     server = Server(config_filepath, mode)
     print(server)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket UDP
+    socket_udp.bind((ip_address, int(port)))
 
-    ip_address = '127.0.0.1'
-    s.bind((ip_address, port))
+    if server.server_type == "SP":
+        threading.Thread(target=server.zone_transfer_sp).start()
+
+    if server.server_type == "SS":
+        threading.Thread(target=server.zone_transfer_ss).start()
 
     print(f"Estou à  escuta no {ip_address}:{port}")
 
     while True:
-        msg, address_from = s.recvfrom(1024)
+        msg, address_from = socket_udp.recvfrom(1024)
         msg = msg.decode('utf-8')
 
         (message_id, flags, name, type_of_value) = parse_message(msg)
@@ -127,18 +148,19 @@ def main():
         server.add_address(message_id, address_from)
 
         if "Q" in flags: # é uma query
-            (bool, response) = server.response_query(msg)
+            response = server.response_query(msg)
 
-            if bool == True:
-                s.sendto(response.encode('utf-8'), address_from)
-            else: # enviar para o seu servidor primario?
+            if response is not None:
+                socket_udp.sendto(response.encode('utf-8'), address_from) # enviar para o destinatário
+            else: #miss
+                return
 
-        else : # é uma reposta duma query e temos de mandar de volta
-            s.sendto(msg.encode('utf-8'), server.get_address(message_id))
+        else: # é uma reposta duma query e temos de mandar de volta
+            socket_udp.sendto(msg.encode('utf-8'), server.get_address(message_id)) # enviar para o destinatário
 
         print(f"Recebi uma mensagem do cliente {address_from}")
 
-    s.close()
+    socket_udp.close()
 
 if __name__ == "__main__" :
     main()
