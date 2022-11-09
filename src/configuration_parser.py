21cef51a236f29180f0a3abeaed4ffabdcc17f00
gabrielasfc <gsfcunha@gmail.com>
2022-11-09 14:10:10 +0000

    created validation class

--- src/configuration_parser.py
+++ src/configuration_parser.py
@@ -1,25 +1,6 @@
+from validation import *
 from primary_server import *
 from secondary_server import *
-
-def validate_port(port):
-    return 1 < int(port) < 65000
-
-
-def validate_ip(ip_address):
-    ip_parts = ip_address.split('.')
-    ip_parts[-1] = (ip_parts[-1].split(':'))[0]
-
-    length = len(ip_address.split(':'))
-    port_bool = True
-
-    if length > 2:
-        port_bool = False
-
-    elif length == 2:
-        port = (ip_address.split(':'))[-1]
-        port_bool = validate_port(port)
-
-    return len(ip_parts) == 4 and all(0 <= int(part) < 256 for part in ip_parts) and port_bool
 
 
 def parser_root_servers(file_path):
@@ -35,6 +16,7 @@ def parser_root_servers(file_path):
     f.close()
 
     return servers
+
 
 def parser_configuration(file_path):
     f = open(file_path, "r")
--- src/database_parser.py
+++ src/database_parser.py
@@ -1,24 +1,5 @@
+from validation import *
 from cache import *
-
-def validate_port(port):
-    return 1 < int(port) < 65000
-
-
-def validate_ip(ip_address):
-    ip_parts = ip_address.split('.')
-    ip_parts[-1] = (ip_parts[-1].split(':'))[0]
-
-    length = len(ip_address.split(':'))
-    port_bool = True
-
-    if length > 2:
-        port_bool = False
-
-    elif length == 2:
-        port = (ip_address.split(':'))[-1]
-        port_bool = validate_port(port)
-
-    return len(ip_parts) == 4 and all(0 <= int(part) < 256 for part in ip_parts) and port_bool
 
 
 def parser_database(file_path):
