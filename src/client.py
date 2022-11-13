0c1de8f39466aa4625b10db6d6bbc4c5ee10de92
MiguelBraga2 <mgdsbraga@gmail.com>
2022-11-13 15:12:18 +0000

    Added documentation in main and client files and removed hard-coded ips

--- src/client.py
+++ src/client.py
@@ -1,8 +1,8 @@
 # Autores: Gabriela Cunha, Guilherme Varela e Miguel Braga
 # Data de criação: 29/10/22
-# Data da última atualização: 11/11/22
+# Data da última atualização: 13/11/22
 # Descrição: Criação de query e envio para o servidor
-# Última atualização: Header
+# Última atualização: Added comments
 
 import socket
 import sys
@@ -17,29 +17,29 @@ def main():
     args_split = args[0].split(":")
 
     message_id = random.randint(1, 65535)
-    ip_address = args_split[0]
-    port = 6000 # porta default
-    domain_name = args[1]
-    type = args[2]
+    ip_address = args_split[0]              # Address of the server
+    port = 6000                             # Default gate of the server
+    domain_name = args[1]                   # Domain of the query (ex: mike.ggm.)
+    type = args[2]                          # Type of entry (ex: A, NS)
     flags = "Q"
 
     if len(args_split) > 1:
-        port = int(args_split[1])
+        port = int(args_split[1])           # If port specified, update port
 
-    if len(args) == 4 and args[3] == "R":
+    if len(args) == 4 and args[3] == "R":   # If recursive mode enabled
         flags = "Q+R"
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #print(message_id)
-    query = DNS(message_id, flags, domain_name, type)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # Creation of a socket UDP
 
-    s.sendto(query.query_to_string().encode('utf-8'), (ip_address, port))
+    query = DNS(message_id, flags, domain_name, type)   # Creation of the query message
+
+    s.sendto(query.query_to_string().encode('utf-8'), (ip_address, port))   # Sending of the query to the chosen server socket
 
     while True:
-        message, add = s.recvfrom(1024)
+        message, add = s.recvfrom(1024)     # Blocks until response received
         message = message.decode('utf-8')
 
-        print(message)
+        print(message) 
 
 
 if __name__ == "__main__" :
--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+# Autores: Gabriela Cunha, Guilherme Varela e Miguel Braga
+# Data de criação: 13/11/22
+# Data da última atualização: 13/11/22
+# Descrição: Receives queries for a given server
+# Última atualização: Added comments
+# TODO: Make it work for multiple servers
+
 import sys
 import threading
 from parse.configuration_parser import *
@@ -6,42 +13,42 @@ from queries.dns import *
 
 def main():
     args = sys.argv
-    config_path = args[1]
-    port = args[2]
-    timeout = args[3]
-    mode = args[4]
-    ip_address = '127.0.0.1'
+    config_path = args[1]       # Path of the config file of the server
+    port = args[2]              # Port the server will be listening
+    timeout = args[3]           # Time the server waits for a response
+    mode = args[4]              # Debug enabled or disabled
 
     if not validate_port(port):
         return  # adicionar log
 
-    server = parser_configuration(config_path, mode)
+    server = parser_configuration(config_path, mode)                        # Parsing the config and database file, creating a server
 
-    socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket UDP
-    socket_udp.bind((ip_address, int(port)))
+    socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)           # Creation of the udp socket
+    ip_address = ""                                                         # Default ip
+    socket_udp.bind((ip_address, int(port)))                                # Binding to server ip
 
-    threading.Thread(target=server.zone_transfer).start()
+    threading.Thread(target=server.zone_transfer).start()                   # New thread for the zone transfer
 
-    print(f"Estou à  escuta no {ip_address}:{port}")
+    #print(f"Estou à  escuta no {ip_address}:{port}")
 
     while True:
-        message, address_from = socket_udp.recvfrom(1024)
+        message, address_from = socket_udp.recvfrom(1024)                   # Receives a message
 
         print(f"Recebi uma mensagem do cliente {address_from}")
 
-        query = string_to_dns(message.decode('utf-8'))
+        query = string_to_dns(message.decode('utf-8'))                      # Decodes and converts to PDU
         #server.log.log_qr(address_from, message) corrigir
 
-        if "Q" in query.flags:  # é queries
-            response = server.interpret_query(query) # objeto DNS
+        if "Q" in query.flags:                                                                          # It is a query
+            response = server.interpret_query(query)                                                    # Create a response to that query
 
-            if "A" in response.flags:
-                socket_udp.sendto(response.query_to_string().encode('utf-8'), address_from) # enviar para o destinatário
-            else:
-                return  # MISS
+            if "A" in response.flags:                                                                   # Answer in cache/DB
+                socket_udp.sendto(response.query_to_string().encode('utf-8'), address_from)             # Send it back
+            else:       
+                return                                                                                  # MISS
 
-        else:  # é uma resposta a uma queries
-            socket_udp.sendto(query.query_to_string().encode('utf-8'), server.get_address(message))
+        else:                                                                                           # It's a response to a query
+            socket_udp.sendto(query.query_to_string().encode('utf-8'), server.get_address(message))     
 
 
     socket_udp.close()
