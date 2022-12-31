ccf204685481ea557ef0e6df326b0fe87b25b296
Guilherme Varela <guilherme.varelaaa@gmail.com>
2022-12-31 14:29:17 +0000

    prints

--- src/server.py
+++ src/server.py
@@ -178,6 +178,7 @@ class Server:
         :return: Endereço do próximo servidor a ser contactado
         """
         authorities_values = Server.sort_by_priority(query.authorities_values)
+        print(authorities_values)
         for record1 in authorities_values:
             if record1.domain in query.domain:
                 for record2 in query.extra_values:
@@ -364,6 +365,7 @@ class Server:
 
             response_code = message.response_code
             next_server = self.find_next_step(message)
+            print(next_server)
             self.change_flags(message)
 
         if response_code == 0:
